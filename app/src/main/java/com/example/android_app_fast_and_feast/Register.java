f26fb81088496ad68ad4dabecf3621b51bf6fc21
Furculesteanu Bianca <48888793+Bia103@users.noreply.github.com>
2021-03-31 14:35:39 +0300

    Added User Page

--- app/src/main/java/com/example/android_app_fast_and_feast/Register.java
+++ app/src/main/java/com/example/android_app_fast_and_feast/Register.java
@@ -1,6 +1,7 @@
 package com.example.android_app_fast_and_feast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
     private TextView textViewRegister;
+    private CardView register_Card_View;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,15 +18,29 @@ public class Register extends AppCompatActivity {
 
 
         textViewRegister = (TextView) findViewById(R.id.logIn);
+
+
         textViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openRegister();
+                openLogIn();
             }
             });
+        register_Card_View = (CardView) findViewById(R.id.register_card_view);
+        register_Card_View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUserPage();
+            }
+        });
+
         }
-        public void openRegister(){
+        public void openLogIn(){
             Intent intent = new Intent(this, LogIn.class);
+            startActivity(intent);
+        }
+        public void openUserPage(){
+            Intent intent = new Intent(this, UserPageActivity.class);
             startActivity(intent);
         }
 
---
+++ app/src/main/java/com/example/android_app_fast_and_feast/UserPageActivity.java
@@ -0,0 +1,54 @@
+package com.example.android_app_fast_and_feast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
+
+public class UserPageActivity extends AppCompatActivity {
+    CircleMenu user_page_circleMenu;
+    RelativeLayout user_page_relative_layout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_page);
+        user_page_circleMenu = findViewById(R.id.user_page_circle_menu);
+        user_page_relative_layout = findViewById(R.id.user_page_relative_layout);
+        ManageCircleMenu();
+    }
+    public void ManageCircleMenu(){
+        user_page_circleMenu.setMainMenu(Color.parseColor("#F4B872"),R.mipmap.hours,R.mipmap.close)
+                .addSubMenu(Color.parseColor("#F4B872"),R.mipmap.restaurant)
+                .addSubMenu(Color.parseColor("#F4B872"),R.mipmap.food_delivery)
+                .addSubMenu(Color.parseColor("#F4B872"),R.mipmap.food_pack)
+                .addSubMenu(Color.parseColor("#F4B872"),R.mipmap.settings)
+                .addSubMenu(Color.parseColor("#F4B872"),R.mipmap.loupe)
+                .setOnMenuSelectedListener(new OnMenuSelectedListener() {
+                    @Override
+                    public void onMenuSelected(int index) {
+                        switch (index){
+                            case 0:
+                                Toast.makeText(UserPageActivity.this, "Restaurant", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 1:
+                                Toast.makeText(UserPageActivity.this, "My purchases", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 2:
+                                Toast.makeText(UserPageActivity.this, "Basket", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 3:
+                                Toast.makeText(UserPageActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                                break;
+                            case 4:
+                                Toast.makeText(UserPageActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                    }
+                });
+    }
+}
